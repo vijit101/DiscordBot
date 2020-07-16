41820dc51e1382e8bd8a6db6e33816cc87d5075c
Mayank Grover <mayank@outscal.com>
2020-07-17 01:11:26 +0530

    reinstall node packages

--- DatabaseSystem/SaveSystem.js
+++ DatabaseSystem/SaveSystem.js
@@ -1,149 +1,149 @@
-var mysql = require('mysql');
-var dotenv = require('dotenv')
-dotenv.config();
+// var mysql = require('mysql');
+// var dotenv = require('dotenv')
+// dotenv.config();
 
-var con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-});
+// var con = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+// });
 
-var SetupSQLDatabase = function SetupSQLDatabase() {
-    console.log("SQLServer tried connecting!");
-    con.connect(function (err) {
-        if (err) {
-            console.log("error")
-            throw err;
-        }
-        con.query("CREATE DATABASE IF NOT EXISTS DiscordBotDatabase", function (err, result) {
-            if (err) { throw err };
-            SetupTables();
-        });
-    });
-}
+// var SetupSQLDatabase = function SetupSQLDatabase() {
+//     console.log("SQLServer tried connecting!");
+//     con.connect(function (err) {
+//         if (err) {
+//             console.log("error")
+//             throw err;
+//         }
+//         con.query("CREATE DATABASE IF NOT EXISTS DiscordBotDatabase", function (err, result) {
+//             if (err) { throw err };
+//             SetupTables();
+//         });
+//     });
+// }
 
-var pool = mysql.createPool({
-    connectionLimit: 30,
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
+// var pool = mysql.createPool({
+//     connectionLimit: 30,
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+// });
 
-function SetupTables() {
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            console.log("error error release"+err);
-            //connection.release();
-            //res.json({ "code": 100, "status": "Error in connection database" });
-            return;
-        }
-        connection.query("CREATE TABLE IF NOT EXISTS UserKarma(UID BIGINT NOT NULL, UserName CHAR(20) NOT NULL,KarmaPoints INT DEFAULT 0, PRIMARY KEY(UID))", function (err, result) {
-            if (err) throw err;
-            console.log("UserKarma table created");
-        });
-        connection.release();
-    });
-}
+// function SetupTables() {
+//     pool.getConnection(function (err, connection) {
+//         if (err) {
+//             console.log("error error release"+err);
+//             //connection.release();
+//             //res.json({ "code": 100, "status": "Error in connection database" });
+//             return;
+//         }
+//         connection.query("CREATE TABLE IF NOT EXISTS UserKarma(UID BIGINT NOT NULL, UserName CHAR(20) NOT NULL,KarmaPoints INT DEFAULT 0, PRIMARY KEY(UID))", function (err, result) {
+//             if (err) throw err;
+//             console.log("UserKarma table created");
+//         });
+//         connection.release();
+//     });
+// }
 
-var CreateUser = function RegisterNewUser(userInfo) {
-    var playerID = "";
-    return new Promise(resolve => {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                connection.release();
-                res.json({ "code": 100, "status": "Error in connection database" });
-                resolve("error");
-            } else {
-                var sql = "INSERT INTO UserKarma(UID,UserName) VALUES ?";
-                var values = [
-                    [userInfo.ID,userInfo.UserName]
-                ];
-                connection.query(sql, [values], function (err, result) {
-                    if (err) throw err;
-                    playerID = result.insertId;
-                    console.log(" New User Added ");
-                    resolve(playerID);
-                });
-            }
-            connection.release();
-        });
-    });
-}
+// var CreateUser = function RegisterNewUser(userInfo) {
+//     var playerID = "";
+//     return new Promise(resolve => {
+//         pool.getConnection(function (err, connection) {
+//             if (err) {
+//                 connection.release();
+//                 res.json({ "code": 100, "status": "Error in connection database" });
+//                 resolve("error");
+//             } else {
+//                 var sql = "INSERT INTO UserKarma(UID,UserName) VALUES ?";
+//                 var values = [
+//                     [userInfo.ID,userInfo.UserName]
+//                 ];
+//                 connection.query(sql, [values], function (err, result) {
+//                     if (err) throw err;
+//                     playerID = result.insertId;
+//                     console.log(" New User Added ");
+//                     resolve(playerID);
+//                 });
+//             }
+//             connection.release();
+//         });
+//     });
+// }
 
-var UpdateKarmaPoints = function Update(UID) {
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            connection.release();
-            res.json({ "code": 100, "status": "Error in connection database" });
-            return ("error");
-        } else {
-            var sql = "UPDATE UserKarma SET KarmaPoints = KarmaPoints + 1 WHERE UID = ? ";
-            connection.query(sql, [UID], function (err, result) {
-                if (err) throw err;
-                console.log(" User's Points Updated"+ result);
-            });
-        }
-        connection.release();
-    });
-}
+// var UpdateKarmaPoints = function Update(UID) {
+//     pool.getConnection(function (err, connection) {
+//         if (err) {
+//             connection.release();
+//             res.json({ "code": 100, "status": "Error in connection database" });
+//             return ("error");
+//         } else {
+//             var sql = "UPDATE UserKarma SET KarmaPoints = KarmaPoints + 1 WHERE UID = ? ";
+//             connection.query(sql, [UID], function (err, result) {
+//                 if (err) throw err;
+//                 console.log(" User's Points Updated"+ result);
+//             });
+//         }
+//         connection.release();
+//     });
+// }
 
-var GetTopKarmaUsers = function GetTopKarmaUsers() {
-    return new Promise(resolve => {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                connection.release();
-                res.json({ "code": 100, "status": "Error in connection database" });
-                resolve("error");
-            } else {
-                var sql = "SELECT * FROM UserKarma ORDER BY KarmaPoints DESC LIMIT 5";
-                connection.query(sql, function (err, result) {
-                    if (err) throw err;
-                    console.log("SQL Data stats" + JSON.stringify(result));
-                    if (Object.keys(result).length > 0) {
-                        var Udata = result;
-                        resolve(Udata);
-                    } else {
+// var GetTopKarmaUsers = function GetTopKarmaUsers() {
+//     return new Promise(resolve => {
+//         pool.getConnection(function (err, connection) {
+//             if (err) {
+//                 connection.release();
+//                 res.json({ "code": 100, "status": "Error in connection database" });
+//                 resolve("error");
+//             } else {
+//                 var sql = "SELECT * FROM UserKarma ORDER BY KarmaPoints DESC LIMIT 5";
+//                 connection.query(sql, function (err, result) {
+//                     if (err) throw err;
+//                     console.log("SQL Data stats" + JSON.stringify(result));
+//                     if (Object.keys(result).length > 0) {
+//                         var Udata = result;
+//                         resolve(Udata);
+//                     } else {
                         
-                        resolve(null);
-                    }
-                });
-            }
-            connection.release();
-        });
-    });
-}
+//                         resolve(null);
+//                     }
+//                 });
+//             }
+//             connection.release();
+//         });
+//     });
+// }
 
-var GetUsersPoints = function GetUserPoints(UID) {
-    return new Promise(resolve => {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                connection.release();
-                res.json({ "code": 100, "status": "Error in connection database" });
-                resolve("error");
-            } else {
-                var sql = "SELECT * FROM UserKarma where UID="+UID;
-                connection.query(sql, function (err, result) {
-                    if (err) throw err;
-                    console.log("SQL Data " + JSON.stringify(result));
-                    if (Object.keys(result).length > 0) {
-                        var Udata = result[0];
-                        resolve(Udata);
-                    } else {
-                        UserID = null;
-                        resolve(UserID);
-                    }
-                });
-            }
-            connection.release();
-        });
-    });
-}
+// var GetUsersPoints = function GetUserPoints(UID) {
+//     return new Promise(resolve => {
+//         pool.getConnection(function (err, connection) {
+//             if (err) {
+//                 connection.release();
+//                 res.json({ "code": 100, "status": "Error in connection database" });
+//                 resolve("error");
+//             } else {
+//                 var sql = "SELECT * FROM UserKarma where UID="+UID;
+//                 connection.query(sql, function (err, result) {
+//                     if (err) throw err;
+//                     console.log("SQL Data " + JSON.stringify(result));
+//                     if (Object.keys(result).length > 0) {
+//                         var Udata = result[0];
+//                         resolve(Udata);
+//                     } else {
+//                         UserID = null;
+//                         resolve(UserID);
+//                     }
+//                 });
+//             }
+//             connection.release();
+//         });
+//     });
+// }
 
-module.exports = {
-    GetUsersPoints,
-    GetTopKarmaUsers,
-    UpdateKarmaPoints,
-    CreateUser,
-    SetupSQLDatabase
-}
+// module.exports = {
+//     GetUsersPoints,
+//     GetTopKarmaUsers,
+//     UpdateKarmaPoints,
+//     CreateUser,
+//     SetupSQLDatabase
+// }
--- Discord_bot.js
+++ Discord_bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const DatabaseSystem = require('./DatabaseSystem/SaveSystem');
+// const DatabaseSystem = require('./DatabaseSystem/SaveSystem');
 const Command = require('./Response/BotCammands.js');
 
 const dotenv = require('dotenv');
@@ -7,7 +7,7 @@ dotenv.config();
 
 const client = new Discord.Client();
 
-DatabaseSystem.SetupSQLDatabase();
+//DatabaseSystem.SetupSQLDatabase();
 var generalChannel;
 
 client.login(process.env.DISCORD_APP_TOKEN); 
@@ -45,59 +45,58 @@ client.on('message', async msg => {
             var reply = await Command.GiveRole(msg);
             SendMessageToChannel(reply, msg.channel.id);
         }
-        else if (msg.content.includes("thank")) {
-            //console.log(msg.mentions.users.array()[0]);
-            for (var i = 0; i < msg.mentions.users.size; i++) {
-                console.log(msg.mentions.users.array()[i].id);
-                if (msg.mentions.users.array()[i].id != msg.author.id) {
-                    DatabaseSystem.UpdateKarmaPoints(msg.mentions.users.array()[i].id);
-                    SendMessageToChannel("Karma point awarded to:" + msg.mentions.user.array()[i].id,msg.channel.id);
-                }
-            }
-        }
+        // else if (msg.content.includes("thank")) {
+        //     //console.log(msg.mentions.users.array()[0]);
+        //     for (var i = 0; i < msg.mentions.users.size; i++) {
+        //         console.log(msg.mentions.users.array()[i].id);
+        //         if (msg.mentions.users.array()[i].id != msg.author.id) {
+        //             DatabaseSystem.UpdateKarmaPoints(msg.mentions.users.array()[i].id);
+        //             SendMessageToChannel("Karma point awarded to:" + msg.mentions.user.array()[i].id,msg.channel.id);
+        //         }
+        //     }
+        // }
     }
-});
- 
+}); 
 
-function SetupBotForChannel(msg){
-    for (var i = 0; i < msg.channel.members.size; i++) {
-        var userInfo = { };
-        if(msg.channel.members.array()[i].nickname!=null){
-            console.log(msg.channel.members.array()[i].nickname)
-            userInfo = {
-                ID: msg.channel.members.array()[i].user.id,
-                UserName: msg.channel.members.array()[i].nickname
-            }
-        }else{
-            console.log(msg.channel.members.array()[i].user.username);
-            userInfo = {
-                ID: msg.channel.members.array()[i].user.id,
-                UserName: msg.channel.members.array()[i].user.username
-            }
-        }
-        DatabaseSystem.CreateUser(userInfo);
-    }
-}
+// function SetupBotForChannel(msg){
+//     for (var i = 0; i < msg.channel.members.size; i++) {
+//         var userInfo = { };
+//         if(msg.channel.members.array()[i].nickname!=null){
+//             console.log(msg.channel.members.array()[i].nickname)
+//             userInfo = {
+//                 ID: msg.channel.members.array()[i].user.id,
+//                 UserName: msg.channel.members.array()[i].nickname
+//             }
+//         }else{
+//             console.log(msg.channel.members.array()[i].user.username);
+//             userInfo = {
+//                 ID: msg.channel.members.array()[i].user.id,
+//                 UserName: msg.channel.members.array()[i].user.username
+//             }
+//         }
+//         DatabaseSystem.CreateUser(userInfo);
+//     }
+// }
 
-function SendAttatchment(link) {
-    // Provide a path to a local file or link
-    const localFileAttachment = new Discord.Attachment(link);
-    generalChannel.send(localFileAttachment);
-}
+// function SendAttatchment(link) {
+//     // Provide a path to a local file or link
+//     const localFileAttachment = new Discord.Attachment(link);
+//     generalChannel.send(localFileAttachment);
+// }
 
-function ListOfServers() {
-    // List servers the bot is connected to
-    console.log("Servers:")
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name);
-        ListOfChannels(guild);
+// function ListOfServers() {
+//     // List servers the bot is connected to
+//     console.log("Servers:")
+//     client.guilds.forEach((guild) => {
+//         console.log(" - " + guild.name);
+//         ListOfChannels(guild);
 
-    })
-}
+//     })
+// }
 
-function ifTagged(receivedMessage) {
-    return receivedMessage.content.includes(client.user.toString())
-}
+// function ifTagged(receivedMessage) {
+//     return receivedMessage.content.includes(client.user.toString())
+// }
 function ListOfChannels(guild) {
     guild.channels.forEach((channel) => {
         console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
