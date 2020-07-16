37abd1b3d1d9f989bbfab3ac18fdcaf8ef313eb6
Mayank Grover <mayank@outscal.com>
2020-07-17 00:32:11 +0530

    added helpful info about google sheet integration

--- Response/BotCammands.js
+++ Response/BotCammands.js
@@ -1,5 +1,6 @@
 DatabaseSystem = require('../DatabaseSystem/SaveSystem');
 const Discord = require('discord.js');
+const GoogleSpreadSheet = require('google-spreadsheet');
 
 var LeaderBoard = function LeaderBoard() {
     return new Promise(async resolve => {
@@ -34,6 +35,8 @@ var GiveRole = function GiveRole(message) {
         // verify the user is on the Target Colleges -> GCStudents sheet (they are in GC!)
         // then only give them the cpp-group-X role 
         // write the email id and role given to Daily Updates -> Roles sheet 
+        // for more info on google sheet integration ref - https://gist.github.com/mayankgrover/1be4978dec294863f14924de5ceecc5b
+
         resolve("Granting role!");
     });
 }
