2a4d86ece91c2f95c015fe88ea4b8f09db2db35a
Shubham <vijit101@gmail.com>
2020-07-17 13:58:21 +0530

    - Added .env dummy file

--- Response/BotCammands.js
+++ Response/BotCammands.js
@@ -31,10 +31,15 @@ var Help = function Help() {
 var GiveRole = function GiveRole(message) { 
     return new Promise(async resolve => {
         // !giverole cpp abc@gmail.com
+        var batchName;
+        var emailID;
+
         // get roles info from google spreadsheet (load it once only!)
+        var roleList;
         // decide which is the next role to be granted to the user 
         // verify the user is on the Target Colleges -> GCStudents sheet (they are in GC!)
-        // then only give them the cpp-group-X role 
+        //load college studs from gc if email == emailgc bool isInSheet  = true
+        // if isInGC true then only give them the cpp-group-X role 
         // write the email id, role given, userid to Daily Updates -> Roles sheet (userid = message.author.id)
         // for more info on google sheet integration ref - https://gist.github.com/mayankgrover/1be4978dec294863f14924de5ceecc5b
         console.log(message);
